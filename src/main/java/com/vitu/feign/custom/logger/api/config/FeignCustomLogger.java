7727d35e1c11aba207f304d34da808a87fd39c2c
Vítor de Santana O Norberto <vithor.norberto@gmail.com>
2023-02-08 00:32:21 -0300

    Adiciona body em formato json ao log

--- src/main/java/com/vitu/feign/custom/logger/api/config/FeignCustomLogger.java
+++ src/main/java/com/vitu/feign/custom/logger/api/config/FeignCustomLogger.java
@@ -1,5 +1,8 @@
 package com.vitu.feign.custom.logger.api.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import feign.Logger;
 import feign.Request;
 import feign.Response;
@@ -7,11 +10,11 @@ import feign.Util;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Objects;
 
 @Slf4j
 public class FeignCustomLogger extends Logger {
+
     @Override
     protected void log(String s, String s1, Object... objects) {
     }
@@ -19,17 +22,17 @@ public class FeignCustomLogger extends Logger {
     @Override
     protected void logRequest(String configKey, Level logLevel, Request request) {
 
-        log.info("-----> Request Begin");
+        log.info("--------------- Request ----------------");
         log.info("-----> Http method: {}", request.httpMethod());
         log.info("-----> Url: {}", request.url());
         log.info("-----> Headers: {}", request.headers());
 
         if (Objects.nonNull(request.body())) {
-            log.info("-----> Request Body: {}", new String(request.body()));
+            String formattedBody = this.getFormattedBody(request.body());
+            log.info("-----> Request Body: \n{}", formattedBody);
         } else {
             log.info("-----> Request Body: ");
         }
-        log.info("-----> Request End");
 
     }
 
@@ -37,16 +40,25 @@ public class FeignCustomLogger extends Logger {
     protected Response logAndRebufferResponse(String configKey, Level logLevel, Response response, long elapsedTime) throws IOException {
 
         byte[] bodyData = Util.toByteArray(response.body().asInputStream());
-        String responseBody = Util.decodeOrDefault(bodyData, Util.UTF_8, "Binary data");
-
-        log.info("<----- Response Begin");
-        log.info("<----- Http method: {}", response.request().httpMethod());
+        log.info("--------------- Response ---------------");
         log.info("<----- Url: {}", response.request().url());
         log.info("<----- Headers: {}", response.headers());
         log.info("<----- Http Status: {}", response.status());
-        log.info("<----- Response Body: {}", responseBody);
-        log.info("<----- Response End");
+
+        if (Objects.nonNull(bodyData) && bodyData.length != 0) {
+            String formattedBody = this.getFormattedBody(bodyData);
+            log.info("<----- Response Body: \n{}", formattedBody);
+        } else {
+            log.info("<----- Response Body: ");
+
+        }
 
         return response.toBuilder().body(bodyData).build();
+    }
+
+    private String getFormattedBody(byte[] body) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().setLenient().create();
+        String responseBody = Util.decodeOrDefault(body, Util.UTF_8, "Binary data");
+        return gson.toJson(JsonParser.parseString(responseBody));
     }
 }
